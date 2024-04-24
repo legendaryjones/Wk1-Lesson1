e1bb559ed614f09718906623d7be4e74777b8fca
Mike <149283058+legendaryjones@users.noreply.github.com>
2024-04-24 00:23:31 -0500

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,53 @@
+#Task 1
+
+'''''
+weather ="sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else: 
+    print ("Take an umbrella!")
+
+    #Task 2
+
+    def feelings():
+     print ("How do you feel today?")("Happy or sad?")
+    if feelings == "Happy":
+        print ("That's great to hear")
+    if feelings == "sad":
+        print ("I hope your day gets better")
+    else:
+        print ("Please enter Happy pr Sad")
+   
+           #Task 3
+        
+Pi_value = "3.14"
+userAge = "25"
+user_location = "New York"
+MAXLIMIT = "1000"
+
+
+#Task 4
+variable_a = "Hello, World!"
+print(type("variable_a"))
+
+variable_b = "23"
+print(type("variable_b"))
+
+variable_c = "3.14"
+print(type("variable_c"))
+
+variable_d = "True"
+print(type("variable_d"))
+
+'''
+
+#Task5
+
+def total():    
+    bread = 3.99
+    pb = 4.99
+    jelly= 3.99
+    return bread + pb + jelly
+total_price= total()
+print ("total price:", total_price)
